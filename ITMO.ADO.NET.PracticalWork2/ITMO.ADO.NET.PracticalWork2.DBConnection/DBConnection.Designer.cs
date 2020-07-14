7da264c8f69fb2d4de2e463c7372d709fe0bddb4
Sergey Sbitnev <sergeysbitnev@mail.ru>
2020-07-14 23:12:39 +0300

    Add PracticalWork2

---
+++ ITMO.ADO.NET.PracticalWork2/ITMO.ADO.NET.PracticalWork2.DBConnection/DBConnection.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace DBConnection
+{
+    partial class DBConnection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.runDB = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectDB = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectDB = new System.Windows.Forms.ToolStripMenuItem();
+            this.asyncConnectionDB = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConnectionList = new System.Windows.Forms.ToolStripMenuItem();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.HowManyProductsButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runDB});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(800, 24);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // runDB
+            // 
+            this.runDB.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectDB,
+            this.disconnectDB,
+            this.asyncConnectionDB,
+            this.ConnectionList});
+            this.runDB.Name = "runDB";
+            this.runDB.Size = new System.Drawing.Size(58, 20);
+            this.runDB.Text = "Run DB";
+            // 
+            // connectDB
+            // 
+            this.connectDB.Name = "connectDB";
+            this.connectDB.Size = new System.Drawing.Size(187, 22);
+            this.connectDB.Text = "Connect DB";
+            this.connectDB.Click += new System.EventHandler(this.openDB_Click);
+            // 
+            // disconnectDB
+            // 
+            this.disconnectDB.Enabled = false;
+            this.disconnectDB.Name = "disconnectDB";
+            this.disconnectDB.Size = new System.Drawing.Size(187, 22);
+            this.disconnectDB.Text = "Disconnect DB";
+            this.disconnectDB.Click += new System.EventHandler(this.closeDB_Click);
+            // 
+            // asyncConnectionDB
+            // 
+            this.asyncConnectionDB.Name = "asyncConnectionDB";
+            this.asyncConnectionDB.Size = new System.Drawing.Size(187, 22);
+            this.asyncConnectionDB.Text = "Async connection DB";
+            this.asyncConnectionDB.Click += new System.EventHandler(this.asyncConnectionDB_Click);
+            // 
+            // ConnectionList
+            // 
+            this.ConnectionList.Name = "ConnectionList";
+            this.ConnectionList.Size = new System.Drawing.Size(187, 22);
+            this.ConnectionList.Text = "Connection list";
+            this.ConnectionList.Click += new System.EventHandler(this.ConnectionList_Click);
+            // 
+            // HowManyProductsButton
+            // 
+            this.HowManyProductsButton.Location = new System.Drawing.Point(12, 27);
+            this.HowManyProductsButton.Name = "HowManyProductsButton";
+            this.HowManyProductsButton.Size = new System.Drawing.Size(129, 23);
+            this.HowManyProductsButton.TabIndex = 1;
+            this.HowManyProductsButton.Text = "How many products";
+            this.HowManyProductsButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // DBConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HowManyProductsButton);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "DBConnection";
+            this.Text = "DBConnection";
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem runDB;
+        private System.Windows.Forms.ToolStripMenuItem connectDB;
+        private System.Windows.Forms.ToolStripMenuItem disconnectDB;
+        private System.Windows.Forms.ToolStripMenuItem asyncConnectionDB;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private BindingSource bindingSource2;
+        private ToolStripMenuItem ConnectionList;
+        private Button HowManyProductsButton;
+        private Label label1;
+    }
+}
+
---
+++ ITMO.ADO.NET.PracticalWork2/ITMO.ADO.NET.PracticalWork2.DBConnection/DBConnection.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using System.Configuration;
+
+namespace DBConnection
+{
+    public partial class DBConnection : Form
+    {
+        SqlConnection connection = new SqlConnection();
+        //string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; Initial Catalog = Northwind; Integrated Security = True";
+
+        public DBConnection()
+        {
+            InitializeComponent();
+            this.connection.StateChange += new StateChangeEventHandler(this.connection_StateChange);
+        }
+
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+
+        string connectionString = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+
+        private void connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            connectDB.Enabled = (e.CurrentState == ConnectionState.Closed);
+            asyncConnectionDB.Enabled = (e.CurrentState == ConnectionState.Closed);
+            disconnectDB.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+
+        private void openDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    connection.Open();
+                    MessageBox.Show("Database connection " + connection.Database + " completed successfully " + "\nServer: " + connection.DataSource);
+                }
+                else
+                    MessageBox.Show("Database connection already established");
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //catch (SqlException XcpSQL)
+            //{
+            //    foreach (SqlError se in XcpSQL.Errors)
+            //    {
+            //        MessageBox.Show(se.Message, "Source of error: " + se.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Ошибка соединения с базой данных");
+            //}
+
+        }
+
+        private void closeDB_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Database connection closed");
+            }
+            else
+                MessageBox.Show("Database connection is already closed");
+        }
+
+        private async void asyncConnectionDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    await connection.OpenAsync();
+                    MessageBox.Show("Database connection " + connection.Database + " completed successfully " + "\nServer: " + connection.DataSource);
+                }
+                else
+                    MessageBox.Show("Database connection already established");
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //catch (SqlException XcpSQL)
+            //{
+            //    foreach (SqlError se in XcpSQL.Errors)
+            //    {
+            //        MessageBox.Show(se.Message, "Source of error: " + se.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Ошибка соединения с базой данных");
+            //}
+        }
+
+        private void ConnectionList_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    string str = String.Format("Name = {0}\nProviderName = {1}\nConnectionString = {2}",
+                        cs.Name, cs.ProviderName, cs.ConnectionString);
+                    MessageBox.Show(str, "Connection settings");
+                }
+            }
+        }
+    }
+}
