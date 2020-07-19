3e2035a8255c65375c8ee0b0a442fea5f95113cf
Sergey Sbitnev <sergeysbitnev@mail.ru>
2020-07-19 10:45:10 +0300

    Add PracticalWork5

---
+++ ITMO.ADO.NET.PracticalWork5/DataBindingSimple/FormDataBindingSimple.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingSimple
+{
+    public partial class FormDataBindingSimple : Form
+    {
+        private BindingSource productsBindingSource;
+        public FormDataBindingSimple()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDataBindingSimple_Load(object sender, EventArgs e)
+        {
+            productsTableAdapter1.Fill(northwindDataSet1.Products);
+            productsBindingSource = new BindingSource(northwindDataSet1, "Products");
+
+            ProductIDTextBox.DataBindings.Add("Text", productsBindingSource, "ProductID");
+            ProductNameTextBox.DataBindings.Add("Text", productsBindingSource, "ProductName");
+
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MoveNext();
+        }
+
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MovePrevious();
+        }
+    }
+}
