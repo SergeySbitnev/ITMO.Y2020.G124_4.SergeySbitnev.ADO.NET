e15030ff385718b2fc8a5def191ca6962463ca68
Sergey Sbitnev <65847001+SergeySbitnev@users.noreply.github.com>
2020-08-01 22:02:12 +0300

    Update PracticalWork0

---
+++ ITMO.ADO.NET.PracticalWork0/PracticalWork0/main.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PracticalWork0
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.WorkingWithDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.VolumeOfSalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.WorkingWithDBToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // WorkingWithDBToolStripMenuItem
+            // 
+            this.WorkingWithDBToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.VolumeOfSalesToolStripMenuItem});
+            this.WorkingWithDBToolStripMenuItem.Name = "WorkingWithDBToolStripMenuItem";
+            this.WorkingWithDBToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.WorkingWithDBToolStripMenuItem.Text = "Работа с БД";
+            // 
+            // VolumeOfSalesToolStripMenuItem
+            // 
+            this.VolumeOfSalesToolStripMenuItem.Name = "VolumeOfSalesToolStripMenuItem";
+            this.VolumeOfSalesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.VolumeOfSalesToolStripMenuItem.Text = "Объем продаж";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 409);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "main";
+            this.Text = "PracticalWork0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem WorkingWithDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem VolumeOfSalesToolStripMenuItem;
+    }
+}
+
